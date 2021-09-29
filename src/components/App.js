f3cf0cab2e6181603580b37a18602f9306c5ed94
bluespringstest <71788357+bluespringstest@users.noreply.github.com>
2021-09-29 18:26:38 +0100

    Rendering for the props

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,36 @@
-import "../styles/App.css";
 import React from "react";
+import PropTypes from "prop-types";
+import LocationDetails from "./LocationDetails";
+import ForecastSummaries from "./ForecastSummaries";
+import "../styles/App.css";
 
-// needs to have return otherwise errors occur.
-function App() {
-  return (
-    <div className="prop">
-      <h1>Weather App</h1>
-    </div>
-  );
-}
+// Step 2 is to import files into the app and render the components. They need to have return otherwise errors occur.
+// changing the component below from this: function App() to: const App = () => makes it into a component that renders components and their props.
+// step 4 is to add the data from the props using PropTypes.
+// step 5 is to add the data into the component as a prop by stating it as a parameter
+const App = ({ location, forecasts }) => (
+  <div className="forecast">
+    <LocationDetails city={location.city} country={location.country} />
+    <ForecastSummaries forecasts={forecasts} />
+  </div>
+);
 
+App.propTypes = {
+  forecasts: PropTypes.arrayOf(
+    PropTypes.shape({
+      date: PropTypes.number,
+      description: PropTypes.string,
+      icon: PropTypes.string,
+      temperature: PropTypes.shape({
+        max: PropTypes.number,
+        min: PropTypes.number,
+      }),
+    })
+  ).isRequired,
+  location: PropTypes.shape({
+    city: PropTypes.string,
+    country: PropTypes.string,
+  }).isRequired,
+};
+// passing in the prop using "<prop /> is the same as passing in  <></>"
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/App.css";
 import App from "./components/App";
-import location from "./data/forecast.json";
+import { location, forecasts } from "./data/forecast.json";
+
+// step 3 is to import external data to the index file THEN passing it to the app.
 
 ReactDOM.render(
   <React.StrictMode>
-    <App location={location} />
+    <App forecasts={forecasts} location={location} />
   </React.StrictMode>,
   document.getElementById("root")
 );
