2d4ac9fe2a45cafc317b4d641bd4ba1076c4043b
bluespringstest <71788357+bluespringstest@users.noreply.github.com>
2021-09-29 18:26:53 +0100

    tests to check correctness

---
+++ src/tests/components/ForecastSummary.test.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import ForecastSummary from "../../components/ForecastSummary";
+
+describe("ForecastSummary", () => {
+    const validProps = {
+        date: 11111111,
+        description: "Stub Description",
+        icon: "stubIcon",
+        temperature: {
+            min: 12,
+            max: 22,
+        },
+    };
+    it("renders the objects correctly in the virtual DOM", () => {
+        const { asFragment } = render(
+            <ForecastSummary 
+            date={validProps.date}
+            description={validProps.description}
+            icon={validProps.icon}
+            temperature={validProps.temperature}
+            />
+        );
+        expect(asFragment).toMatchSnapshot();
+    });
+    it("renders the correct values for the props", () => {
+        const { getByText, getByTestId } = render(
+            <ForecastSummary 
+            date={validProps.date}
+            description={validProps.description}
+            icon={validProps.icon}
+            temperature={validProps.temperature}
+            />
+        );
+        expect(getByText("11111111")).toHaveClass("forecast-summary__date");
+        expect(getByText("Stub Description")).toHaveClass("forecast-summary__description");
+        expect(getByTestId("forecast-icon")).toHaveClass("forecast-summary__icon");
+        expect(getByText("22°C")).toHaveClass("forecast-summary__temperature");
+    });
+});
